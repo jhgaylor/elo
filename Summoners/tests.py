b2015dc85e7d817447d3d74ee580da9fdd099fe1
Jake Gaylor <jhgaylor@gmail.com>
2012-11-11 16:18:32 -0600

    added first test ever

--- Summoners/tests.py
+++ Summoners/tests.py
@@ -20,10 +20,11 @@ class SetupTest(TestCase):
 			s.region = n['region']
 			s.save()
 			sleep(1)
+		
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+# class SimpleTest(TestCase):
+#     def test_basic_addition(self):
+#         """
+#         Tests that 1 + 1 always equals 2.
+#         """
+#         self.assertEqual(1 + 1, 2)
