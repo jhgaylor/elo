31300ed0040b8a2c9bb63b743b5c36997097910d
Jake Gaylor <jhgaylor@gmail.com>
2012-11-11 13:19:31 -0600

    added urlencode

--- Summoners/views.py
+++ Summoners/views.py
@@ -1,10 +1,11 @@
 from Summoners.models import Summoner
 import requests
 import json
+import urllib
 # Create your views here.
 API_KEY = "ormTSJxcEMPj9kJA0p3B"
 def get_summoner(region, name):
-	url = "http://elophant.com/api/v1/"+region+"/getSummonerByName?summonerName="+name+"&key="+API_KEY
+	url = "http://elophant.com/api/v1/"+region+"/getSummonerByName?summonerName="+urllib.urlencode(name)+"&key="+API_KEY
 	r = requests.get(url)
 	data = r.json
 	
