d353f04acf92843cf24bbda7a4172f3817999ac5
Jake Gaylor <jhgaylor@gmail.com>
2012-11-11 16:16:04 -0600

    added first test ever

--- Summoners/models.py
+++ Summoners/models.py
@@ -18,7 +18,7 @@ class Summoner(models.Model):
 	summonerId 		= models.IntegerField()
 	dataVersion 	= models.IntegerField()
 
-	#SAMPLE_NAMES = ["redrangerftw","Vephoma","Dergonborn","AntiSlash","devilboy945","Dragon1kill","FaceRollR","MasterChieft","Banana111","DerekSucksAtLoL","tc Chimera","davekiller123","LolMe","WangsBeefIsGood","Xbee","tidalbutt","Arcner","syvarious","zark95","KamzX","D O M I N A T 3","thekler","xiaocong","Yendog","Mr Tanks Alot","RufflesRidges","EvilBreed","Lil Venom21","Skipoix","HIghGamingCk","Digital Firewall","Free Champs 0nly","BazookaJay","Funkmaster88","Ryzen Legacy","kwondot","DestroyAllThatsG","RN5xxReckless","VyzVengance","Enevish","Emo Killz","Vakuous","PurpleTabs","KrystalAnn","2 Drunk 2 Aim","Zephrax","Chef The Tony","RaiDiengSaz","CaptainZero0","Clyde Real Ez","Verkato","OREALE","TrueBornSolja","Bowtie Bandito","LOLciferHAHA","cattlepillar","royalstyles","mashpotatoes","LowKey937","haozi","VietDavis","Duy lam12","AllHailTheQueen","Nick1201","Xetene","I Went To Jared","xXicewolfbladeXx","thunderaine","Deblujack13","legendairy251","esceptico","MrGan13","Deadkher","sio00","Biersack","Cowser","killerhurts","EpikS","Bargosa","MystBlanco","dubduce262","aundria","HornyRhino69","Kn3Gr0","Eddy2010","LIPPS","SylvanasWrunner","Mewsers","KsHMetal","Twin Performance","Flowzart","axiommm","Onlyplastic","Baseball Skillz","PokeMyButtPlz","WonJohnWon","Photochronograph","busycoding","MathomaticDimsim","Skyfreak3","Sycip","DiabloHellfire","IonianNirvana","fugle","Lyych","Jseinfeld","Raulstryker","Doi Toshikatsu","Android Four","GR3yZy","jelmod","ikoosh","2od","xTinchix","TacoandBurritos","SteliosV","supermonkeyteam","bato2","ShaCrystal","YMCMBallday","ErrorInSystem","Exzirion","Ianbearpig1337","bctech","stilla","Funybn tic tac","BlastNasy","mistlord","EroSinGoD","GrAViToR99","Schecter28","thekillerofpeopl","snpdng","Joybane","Hafahoodz","DrakoxCR","Rick271","TheOmnipotentZ","BlancaTaza","ADD2012","Epixs","CrowOZ","NeverBackDown2","whatshield","Knightmare Pk","xXAcursedXx","jetpotion","Sw00Sh","BallBagger","Outaylor","Imbapwn","Xcr","deLiTy","LSD and Ecstasy"]
+
 
 	#requires self.name and self.region to be set.
 	def get(self):
@@ -38,6 +38,7 @@ class Summoner(models.Model):
 	#http://stackoverflow.com/questions/377454/how-do-i-get-my-python-program-to-sleep-for-50-msec
 
 class RankedChampionStats(models.Model):
+	summoner 			= models.ForeignKey(Summoner)
 	championId 			= models.ForeignKey(Champion)
 	wins 				= models.IntegerField()
 	losses 				= models.IntegerField()
--- Summoners/views.py
+++ Summoners/views.py
@@ -1,7 +1,1 @@
 from Summoners.models import Summoner
-import requests
-import json
-import urllib
-# Create your views here.
-
-
