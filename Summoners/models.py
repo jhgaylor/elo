0e43e308d553a74327874667026160819ca86435
togoshigekata <togoshigekata@gmail.com>
2012-11-11 14:12:42 -0600

    Update Summoners/models.py

--- Summoners/models.py
+++ Summoners/models.py
@@ -3,12 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Summoner(models.Model):
-	name = models.CharField(max_length=200)
-	internalName = models.CharField(max_length=200)
-	acctId = models.IntegerField()
+	
+	name          = models.CharField(max_length=200)
+	internalName  = models.CharField(max_length=200)
+	acctId        = models.IntegerField()
 	profileIconId = models.IntegerField()
-	revisionId = models.IntegerField()
+	revisionId    = models.IntegerField()
 	summonerLevel = models.IntegerField()
-	summonerId = models.IntegerField()
-	dataVersion = models.IntegerField()
+	summonerId    = models.IntegerField()
+	dataVersion   = models.IntegerField()
 	
