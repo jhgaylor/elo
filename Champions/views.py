eb322a1212a75b1fa9c7dad380d3fd1e1ba8a49b
Jake Gaylor <jhgaylor@gmail.com>
2012-11-11 12:43:27 -0600

    add get_champions

--- Champions/views.py
+++ Champions/views.py
@@ -1,4 +1,10 @@
 from Champions.models import Champion
+import requests
+import json
 # Create your views here.
+API_KEY = "ormTSJxcEMPj9kJA0p3B"
 def get_champions():
-	print "here"
+	url = "http://elophant.com/api/v1/champions?key="+API_KEY
+	r = requests.get(url)
+	data = json.loads(r.json)
+	print data
