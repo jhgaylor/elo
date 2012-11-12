16525a87873580ef554d7bd85f023670c3b8b015
Jake Gaylor <jhgaylor@gmail.com>
2012-11-12 09:26:21 -0600

    Added elophant api wrapper

--- Summoners/models.py
+++ Summoners/models.py
@@ -1,7 +1,7 @@
 from elo import settings
 from django.db import models
 from Champions.models import Champion
-
+from elophant.api import api
 import requests
 import json
 import urllib
@@ -17,38 +17,43 @@ class Summoner(models.Model):
 	summonerLevel 	= models.IntegerField()
 	summonerId 		= models.IntegerField()
 	dataVersion 	= models.IntegerField()
+	api 			= api(key=settings.ELO_API_KEY)
 
 	#requires self.name and self.region to be set.
 	def get(self):
-		if (self.name == "") or (self.region == ""):
-			return False #if either required parameter is unavailable to us we return false
-		url = "http://elophant.com/api/v1/"+self.region+"/getSummonerByName?summonerName="+urllib.quote(self.name)+"&key="+settings.ELO_API_KEY
-		r = requests.get(url) #send a get request for the response
-		data = r.json #sets data to the object represented by the json response
-		if data is not None: #make sure we got a response.  The response will be none if they have no data for the request
-			for k, v in data.iteritems(): #iterate over the response json as key value pairs
-				if hasattr(self, k): #check if the object has an attribute with the same name as the key
-					setattr(self, k, v) #if it does, set the value of the attribute to the value of key
-			self.save() #save the object
+		try:
+			data = self.api.summoner_by_name(self.name, self.region) #sets data to the object represented by the json response
+			if data is None:
+				raise TypeError
+		except:
+			return False
+		
+		for k, v in data.iteritems(): #iterate over the response json as key value pairs
+			if hasattr(self, k): #check if the object has an attribute with the same name as the key
+				setattr(self, k, v) #if it does, set the value of the attribute to the value of key
+		self.save() #save the object to the database
 
 	def get_ranked_stats(self):
-		url = "http://elophant.com/api/v1/"+self.region+"/getRankedStats?accountId="+str(self.acctId)+"&season=CURRENT&key="+settings.ELO_API_KEY
-		r = requests.get(url) #send a get request for the response
-		data = r.json #sets data to the object represented by the json response
-		if data is not None: #make sure we got a response.  The response will be none if they have no data for the request
-			for each in data:
-				try:
-					stat = RankedChampionStats.objects.get(ChampionId=each['ChampionId'], summoner=self)
-				except:
-					stat = RankedChampionStats()
-					stat.summoner = self
-				for k, v in each.iteritems(): #iterate over the response json as key value pairs
-					if k == 'ChampionId':
-						setattr(stat, k, Champion.objects.get(pk=v))
-					else:
-						if hasattr(stat, k): #check if the object has an attribute with the same name as the key
-							setattr(stat, k, v) #if it does, set the value of the attribute to the value of key
-				stat.save() #save the object
+		try:
+			data = self.api.get_ranked_stats(self.acctId, self.region)
+			if data is None:
+				raise TypeError
+		except:
+			return False
+	
+		for each in data:
+			try:
+				stat = RankedChampionStats.objects.get(ChampionId=each['ChampionId'], summoner=self)
+			except:
+				stat = RankedChampionStats()
+				stat.summoner = self
+			for k, v in each.iteritems(): #iterate over the response json as key value pairs
+				if k == 'ChampionId':
+					setattr(stat, k, Champion.objects.get(pk=v))
+				else:
+					if hasattr(stat, k): #check if the object has an attribute with the same name as the key
+						setattr(stat, k, v) #if it does, set the value of the attribute to the value of key
+			stat.save() #save the object
 
 
 	#http://stackoverflow.com/questions/377454/how-do-i-get-my-python-program-to-sleep-for-50-msec
